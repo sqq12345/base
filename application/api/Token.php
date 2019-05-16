cd132af89a6ffd91655e6400afcf7f9c9e32b110
skywalkerwei <skywalkerwei@gmail.com>
2019-05-16 10:22:22 +0800

    <新功能>: (api接口) <添加单点登录设置>

--- application/api/Token.php
+++ application/api/Token.php
@@ -4,10 +4,12 @@ namespace app\api;
 use \Firebase\JWT\JWT;
 use think\Request;
 use app\api\exception\TokenException;
-
+use think\Cache;
 
 class Token
 {
+
+   protected static $needOOS = 0; //是否需要单点登陆
     /**
      * 签名方法
      */
@@ -15,22 +17,30 @@ class Token
     {
 
         $key =  self::generateTokenKey();
-
+        $nowTime = time();
         $token = [
-            "iss"   => "centos",            // 签发者
-            "iat"   => time(),              // 签发时间
-            "exp"   => time() + 30*24*60*60,// 过期时间
-            "aud"   => 'web',               // 接收方
-            "sub"   => 'all',               // 面向的用户
+            "iss"   => "centos",              // 签发者
+            "iat"   => $nowTime,              // 签发时间
+            "exp"   => $nowTime + 30*24*60*60,// 过期时间
+            "aud"   => 'web',                 // 接收方
+            "sub"   => 'all',                 // 面向的用户
+            "jti"   => md5($nowTime),
             "data"  => $data
         ];
         $jwt = JWT::encode($token, $key);
-        $data['user_token'] = $jwt;
+        $data['token'] = $jwt;
+        if(self::$needOOS){
+          if(array_key_exists('uid',$data)){
+            $uid = $data['uid'];
+            Cache::set($uid,$token['jti']);
+          }
+        }
+        
         return $data;
     }
 
     // 解密user_token
-    public  static function decoded_user_token()
+    public  static function decodedToken()
     {
         $key =  self::generateTokenKey();
         $token = Request::instance()->header('token');
@@ -43,45 +53,61 @@ class Token
         throw new TokenException();
     }
 
-    //获取uid
+    //校验数据
+    public  static function verification($result)
+    {
+      // 有效时间
+      if (!(bool)($result["exp"] - time())) {
+        throw new TokenException();
+      }
+      $user = json_decode(json_encode($result['data']), true);
+        //单点登陆
+      if(self::$needOOS){
+          if(array_key_exists('uid',$user)){
+            $uid = $user['uid'];
+            $uJti = Cache::get($uid);
+            //对比 jti 
+            if($result["jti"] != $uJti){
+              throw new TokenException();
+            }
+          }
+       }
+       return $user;
+    }
+
+     //安全的获取uid 不需要校验
+     public static function safe_uid()
+     {
+         $uid = 0;
+         $key =  self::generateTokenKey();
+         $token = Request::instance()->header('token');
+         try{
+             $result = (array)JWT::decode($token,$key, array('HS256'));
+             if ((bool)($result["exp"] - time())) {
+                 $user = json_decode(json_encode($result['data']), true);
+                 return $user['uid'];
+             }
+         }catch(\Exception $e){
+            
+         }
+         return $uid;
+     }
+
+
+    //获取 - uid
     public  static function uid()
     {
-        $result =self::decoded_user_token();
-        if ((bool)($result["exp"] - time())) {
-            $user = json_decode(json_encode($result['data']), true);
-            return $user['uid'];
-        }
-        throw new TokenException();
+        $result =self::decodedToken();
+        $user = self::verification();
+        return $user['uid'];
     }
 
-    //安全的获取uid
-    public  static function safe_uid()
-    {
-        $uid = 0;
-        $key =  self::generateTokenKey();
-        $token = Request::instance()->header('token');
-        try{
-            $result = (array)JWT::decode($token,$key, array('HS256'));
-            if ((bool)($result["exp"] - time())) {
-                $user = json_decode(json_encode($result['data']), true);
-                return $user['uid'];
-            }
-        }catch(\Exception $e){
-
-        }
-        return $uid;
-    }
-
-    //获取userInfo
+   
+    //获取 - userInfo
     public static function userInfo()
     {
-        $result =self::decoded_user_token();
-        if (count($result)) {
-            if ((bool)($result["exp"] - time())) {
-                return json_decode(json_encode($result['data']), true);
-            }
-        }
-        throw new TokenException();
+      $result =self::decodedToken();
+      return self::verification();
     }
 
     // 生成key
